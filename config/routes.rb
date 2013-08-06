a4a9bf1df018331639336bc8a8563f60a2a5265b
wrightling <warren@thewrightangle.com>
2013-08-06 11:18:36 -0600

    added flatuipro demo page/controller/assets

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Watchman::Application.routes.draw do
+  get "flatuipro_demo/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
