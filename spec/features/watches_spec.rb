45ce128c4a57d0263f0386fb27f0a0a7ed97d106
wrwright <warren@thewrightangle.com>
2013-08-22 12:17:07 -0600

    feature spec will generate test data and verify it shows on page

--- spec/features/watches_spec.rb
+++ spec/features/watches_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 
 feature "GET /" do
+  before :each do
+    5.times { create(:event) }
+  end
+
   scenario "Page title is 'Watchman'" do
     visit root_path
     expect(page).to have_title "Watchman"
@@ -8,6 +12,6 @@ feature "GET /" do
 
   scenario "events show in page body" do
     visit root_path
-    expect(page).to have_content "SCHEDULABLE: calling"
+    expect(page).to have_content Event.first.formatted_message
   end
 end
