369d283086cee64ca9d613079b6ad84dda18fd01
wrwright <warren@thewrightangle.com>
2013-09-06 08:51:01 -0600

    updated script to seed a more interesting message

--- db/scripts/insert_logging_events.sql
+++ db/scripts/insert_logging_events.sql
@@ -14,7 +14,7 @@ union select 6, '2013-05-07 07:27:44', "Message 6", "INFO", "Timer-1", "com.nwli
 go
 
 insert into logging_event
-      select 7, '2013-05-08 07:27:44', "Message 7", "INFO", "Timer-1", "com.nwlinc.contentmanager.jobs.jboss.schedule.CacheEngineSchedulable", "CacheEngineSchedulable.java", "perform", "400"
+      select 7, '2013-05-08 07:27:44', "Message 7 - Twas Brillig and the slithy toves did gyre and gimble in the wabe.  All mimsy were the borogroves, and the mome wraths outgbrabe.  Beware the Jabberwock my son, the jaws the bite, the claws that catch.", "INFO", "Timer-1", "com.nwlinc.contentmanager.jobs.jboss.schedule.CacheEngineSchedulable", "CacheEngineSchedulable.java", "perform", "400"
 union select 8, '2013-05-09 07:27:44', "Message 8", "INFO", "Timer-1", "com.nwlinc.contentmanager.jobs.jboss.schedule.CacheEngineSchedulable", "CacheEngineSchedulable.java", "perform", "9999"
 go
 
