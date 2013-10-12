8ecce50095a893f24e345b7e4d6f2bfaf6a03546
wrwright <warren@thewrightangle.com>
2013-10-11 18:53:11 -0600

    additional test data for the dev database

--- db/scripts/insert_logging_events.sql
+++ db/scripts/insert_logging_events.sql
@@ -18,5 +18,15 @@ insert into logging_event
 union select 8, '2013-05-09 07:27:44', "Message 8", "WARN", "Timer-1", "com.nwlinc.contentmanager.jobs.jboss.schedule.CacheEngineSchedulable", "CacheEngineSchedulable.java", "perform", "9999", "com.nwlinc.contentmanager.jobs.jboss.schedule.CacheEngineSchedulable"
 go
 
+insert into logging_event
+      select 9, '2013-05-10 07:27:44', "Message 9 - Twas Brillig and the slithy toves did gyre and gimble in the wabe.  All mimsy were the borogroves, and the mome wraths outgbrabe.  Beware the Jabberwock my son, the jaws the bite, the claws that catch.", "INFO", "Timer-1", "com.nwlinc.contentmanager.jobs.jboss.schedule.CacheEngineSchedulable", "CacheEngineSchedulable.java", "perform", "400", "com.nwlinc.contentmanager.jobs.jboss.schedule.CacheEngineSchedulable"
+union select 10, '2013-05-11 07:27:44', "Message 10", "WARN", "Timer-1", "com.nwlinc.contentmanager.jobs.jboss.schedule.CacheEngineSchedulable", "CacheEngineSchedulable.java", "perform", "9999", "com.nwlinc.contentmanager.jobs.jboss.schedule.CacheEngineSchedulable"
+union select 11, '2013-05-12 07:27:44', "Message 11", "WARN", "Timer-1", "com.nwlinc.contentmanager.jobs.jboss.schedule.CacheEngineSchedulable", "CacheEngineSchedulable.java", "perform", "9999", "com.nwlinc.contentmanager.jobs.jboss.schedule.CacheEngineSchedulable"
+union select 12, '2013-05-13 07:27:44', "Message 12", "WARN", "Timer-1", "com.nwlinc.contentmanager.jobs.jboss.schedule.CacheEngineSchedulable", "CacheEngineSchedulable.java", "perform", "9999", "com.nwlinc.contentmanager.jobs.jboss.schedule.CacheEngineSchedulable"
+union select 13, '2013-05-14 07:27:44', "Message 13", "WARN", "Timer-1", "com.nwlinc.contentmanager.jobs.jboss.schedule.CacheEngineSchedulable", "CacheEngineSchedulable.java", "perform", "9999", "com.nwlinc.contentmanager.jobs.jboss.schedule.CacheEngineSchedulable"
+union select 14, '2013-05-15 07:27:44', "Message 14", "WARN", "Timer-1", "com.nwlinc.contentmanager.jobs.jboss.schedule.CacheEngineSchedulable", "CacheEngineSchedulable.java", "perform", "9999", "com.nwlinc.contentmanager.jobs.jboss.schedule.CacheEngineSchedulable"
+union select 15, '2013-05-16 07:27:44', "Message 15", "WARN", "Timer-1", "com.nwlinc.contentmanager.jobs.jboss.schedule.CacheEngineSchedulable", "CacheEngineSchedulable.java", "perform", "9999", "com.nwlinc.contentmanager.jobs.jboss.schedule.CacheEngineSchedulable"
+go
+
 delete from logging_event
 go
